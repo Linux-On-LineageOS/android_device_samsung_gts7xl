3c0b2976adc5ae5fa3ee7dc45880dc8c71030c31
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:22:51 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -50,3 +50,4 @@ $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:
 
 # Inherit from vendor blobs
 $(call inherit-product-if-exists, vendor/samsung/gts7xl/gts7xl-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
